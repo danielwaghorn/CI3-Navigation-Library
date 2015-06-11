3f5e055ce11268fe7feed0cdc322593636d684f9
Daniel Waghorn <daniel@daniel-waghorn.co.uk>
2015-06-11 11:54:55 +0100

    Added two configs
    Added a config template and one for Foundation Top Bar

---
+++ config/navigation_foundation.php
@@ -0,0 +1,46 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * $config file for CI3-Navigation-Library and Zurb Foundation.
+ * User: danielwaghorn
+ * Date: 11/06/15
+ * Time: 11:23
+ *
+ * @var navigation_open:
+ *      Contains the open tag e.g. <ul class="nav">
+ * @var navigation_close:
+ *      Contains the closing tag e.g. </ul>
+ * @var item_open:
+ *      Contains the open tag for a nav item e.g. <li>
+ * @var item_open_active:
+ *      Contains the open tag for an active item e.g.
+ *      <li class="active">
+ * @var item_close:
+ *      Contains the close tag for an item e.g. </li>
+ * @var anchor:
+ *      Contains the template for an anchor in an item
+ *      e.g. <a href="{$url}" {$extra}>{$text}</a>
+ *      where $url is the link to the item,
+ *      $extra is any additional attributes e.g. class="main"
+ *      and $text is the text to be held in the anchor.
+ * @var dropdown_open:
+ *      Contains the open tag for a dropdown e.g. <ul class="dropdown">
+ * @var dropdown_close:
+ *      Contains the close tag for a dropdown e.g. </ul>
+ */
+
+
+$config['navigation_open'] = '<ul class="right">';
+
+$config['navigation_close'] = '</ul>';
+
+$config['item_open'] = '<li>';
+
+$config['item_open_active'] = '<li class="active">';
+
+$config['item_close'] = '</li>';
+
+$config['anchor'] = '<a href="{$url}" {$extra}>{$text}</a>';
+
+$config['dropdown_open'] = '<ul class="has-dropdown">';
+
+$config['dropdown_close'] = '</ul>';
