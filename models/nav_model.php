9a64d856633fa8499cec11dc05001be2400f281c
Daniel Waghorn <daniel@daniel-waghorn.co.uk>
2015-06-11 11:54:04 +0100

    Tidied up Model

--- models/nav_model.php
+++ models/nav_model.php
@@ -10,10 +10,6 @@
 
 class Nav_model extends CI_Model {
 
-    var $title   = '';
-    var $content = '';
-    var $date    = '';
-
     function __construct()
     {
         // Call the Model constructor
